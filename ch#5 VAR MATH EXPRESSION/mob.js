53d9f3d7d3f8f64dbbbbe455949582115fff0774
Mustafapervaiz1 <121414670+Mustafapervaiz1@users.noreply.github.com>
2023-02-05 21:54:16 +0500

    ch 5 update

--- ch#5 VAR MATH EXPRESSION/mob.js
+++ ch#5 VAR MATH EXPRESSION/mob.js
@@ -1,18 +1,27 @@
-var add = 4+5
-document.write(add)
+
+                 //  ************* MATH EXPRESSION ADDITION *******************
+ var a = Number(prompt("write firt number for addition"));
+ var b = Number(prompt("write second number for addition"));
+ alert(a+b)
+  document.write(` Sum of ${a} and ${b} is ${a+b}`)("\n")
+                 // ************* MATH EXPRESSION SUBTRACTION *******************
+ var a = Number(prompt("write a first number for subtraction"));
+var b = Number(prompt("write a second number for subtraction"));
+ alert(a-b)
+  document.write(`Subtract ${a} from ${b} is ${a-b}`)
+                //  ************* MATH EXPRESSION multiplication *******************
+var a = Number(prompt("write a First number for multiply"))
+var b = Number(prompt("write a Second number for multiply"))
+alert(a*b)
+document.write(`${a} multiply by ${b} is :${a*b}`)
+
+                           //  ************* MATH EXPRESSION division *******************
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-                // ******************** ASSIGNMENT COMPLETE ********************
+var a = Number(prompt("write a First number for division"))
+var b = Number(prompt("write a Second number for division"))
+alert(a/b)
+document.write(`${a} divide by ${b} is :${a/b}`)
+//  ************* MATH EXPRESSION   modulus later *******************
+//  ************* MATH EXPRESSION ADDITION *******************
+//  ************* MATH EXPRESSION ADDITION *******************
