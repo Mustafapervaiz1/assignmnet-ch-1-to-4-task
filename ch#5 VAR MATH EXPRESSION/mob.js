d2d0c4f1513ce9f540af207bba9e15538b2e527d
mustafapervez <syalani.training1@gmail.com>
2023-02-27 22:58:29 +0500

    increament and decrement

--- ch#5 VAR MATH EXPRESSION/mob.js
+++ ch#5 VAR MATH EXPRESSION/mob.js
@@ -1,27 +1,57 @@
 
-                 //  ************* MATH EXPRESSION ADDITION *******************
- var a = Number(prompt("write firt number for addition"));
- var b = Number(prompt("write second number for addition"));
- alert(a+b)
-  document.write(` Sum of ${a} and ${b} is ${a+b}`)("\n")
-                 // ************* MATH EXPRESSION SUBTRACTION *******************
- var a = Number(prompt("write a first number for subtraction"));
-var b = Number(prompt("write a second number for subtraction"));
- alert(a-b)
-  document.write(`Subtract ${a} from ${b} is ${a-b}`)
-                //  ************* MATH EXPRESSION multiplication *******************
-var a = Number(prompt("write a First number for multiply"))
-var b = Number(prompt("write a Second number for multiply"))
-alert(a*b)
-document.write(`${a} multiply by ${b} is :${a*b}`)
+//                  //  ************* MATH EXPRESSION ADDITION *******************
+  var a = Number(prompt("write firt number for addition"));
+  var b = Number(prompt("write second number for addition"));
 
-                           //  ************* MATH EXPRESSION division *******************
+   document.write(` Sum of ${a} and ${b} is ${a+b}<br>`)
+// //                  // ************* MATH EXPRESSION SUBTRACTION *******************
+  var c = Number(prompt("write a first number for subtraction"));
+  var d = Number(prompt("write a second number for subtraction"));
+
+    document.write(`Subtract ${c} from ${d} is ${c-d}</br>`)
+//                  //  ************* MATH EXPRESSION multiplication *******************
+ var e = Number(prompt("write a First number for multiply"))
+  var f = Number(prompt("write a Second number for multiply"))
+ 
+  document.write(`${e} multiply by ${f} is :${e*f}</br>`)
+
+//                             //  ************* MATH EXPRESSION division *******************
 
 
-var a = Number(prompt("write a First number for division"))
-var b = Number(prompt("write a Second number for division"))
-alert(a/b)
-document.write(`${a} divide by ${b} is :${a/b}`)
-//  ************* MATH EXPRESSION   modulus later *******************
-//  ************* MATH EXPRESSION ADDITION *******************
-//  ************* MATH EXPRESSION ADDITION *******************
+  var g = Number(prompt("write a First number for division"))
+  var h = Number(prompt("write a Second number for division"))
+
+document.write(`${g} divide by ${h} is ${g/h}</br>`)
+ // //  ************* MATH EXPRESSION   modulus later                     nooooooooooooooooooot *******************
+  //var m = Number(prompt("write a First number for modulus"))
+  ///var o = Number(prompt("write a Second number for modulus"))
+
+ //3a. Declare a varriable
+var items;
+
+// 3b show value
+document.write(`value after decleration is ${items}<br>`)
+
+
+// 3c initliaze a value
+items = 5
+
+//3d 
+document.write(`Initial value is: ${items}<br>`)
+
+items++;
+
+//increament a varriable
+document.write(`6 value after increment  is ${items}<br>`)
+
+items = items + 7;
+
+document.write(`Value after addition is ${items}<br>`)
+
+items--;
+
+
+items = items % 3;
+
+document.write(`Remainder is ${items}<br>`)
+
